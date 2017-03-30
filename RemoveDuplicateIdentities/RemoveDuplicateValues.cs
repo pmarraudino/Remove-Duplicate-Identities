60d618b04a5f7adb839383f3a90842b2eb0d9203
pmarraud <pmarraud@S32279>
2017-03-30 13:22:40 -0500

    Removed a Line and Cleaned

--- RemoveDuplicateIdentities/RemoveDuplicateValues.cs
+++ RemoveDuplicateIdentities/RemoveDuplicateValues.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RemoveDuplicateIdentities
 {
@@ -15,19 +13,17 @@ namespace RemoveDuplicateIdentities
             var uniqueLines = new List<Tuple<string, string, string>>();
             foreach (string line in allLines)
             {
-                var columns = line.Split(',');
-                if (uniqueLines.FirstOrDefault(currentLine => currentLine.Item1 == columns[0] || currentLine.Item3 == columns[2]) == null)
+                var column = line.Split(',');
+                if (uniqueLines.FirstOrDefault(currentLine => currentLine.Item1 == column[0] || currentLine.Item3 == column[2]) == null)
                 {
-                    uniqueLines.Add(new Tuple<string, string, string>(columns[0], columns[1], columns[2]));
+                    uniqueLines.Add(new Tuple<string, string, string>(column[0], column[1], column[2]));
                 }
-
             }
 
             using (StreamWriter writer = new StreamWriter(File.Open(PathFinder.FilePath("EmpId_dupRmv.csv"), FileMode.Create)))
                 foreach (var item in uniqueLines)
                 {
                     writer.WriteLine($"{item.Item1}, {item.Item2}, {item.Item3}");
-                    Console.WriteLine($"{item.Item1}, {item.Item2}, {item.Item3}");
                 }
         }
 
